4772377873b8c2e3ae9379bcbae2f1e418352f56
Richard <richardfadhilah120@gmail.com>
2025-09-03 08:22:11 +0700

    0.2

--- note-app/src/Components/Todo.jsx
+++ note-app/src/Components/Todo.jsx
@@ -1,14 +1,55 @@
+import { useEffect, useRef, useState } from "react";
 import "./CSS/Todo.css";
+import Todoitems from "./Todoitems";
 
 const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  let count = 0;
+
+  const add = () => {
+    setTodos([
+      ...todos,
+      { no: count++, text: inputRef.current.value, display: "" },
+    ]),
+      (inputRef.current.value = "");
+  };
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
   return (
     <div className="todo">
       <div className="todo-header">To-Do List</div>
       <div className="todo-add">
-        <input type="text" placeholder="Add Your Task" className="todo-input" />
-        <div className="todo-add-btn">ADD</div>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Add Your Task"
+          className="todo-input"
+        />
+        <div
+          onClick={() => {
+            add();
+          }}
+          className="todo-add-btn"
+        >
+          ADD
+        </div>
       </div>
-      <div className="todo-list"></div>
+      <div className="todo-list">
+        {todos.map((item, index) => {
+          return (
+            <Todoitems
+              key={index}
+              no={item.no}
+              display={item.display}
+              text={item.text}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- note-app/src/Components/Todoitems.jsx
+++ note-app/src/Components/Todoitems.jsx
@@ -1,7 +1,19 @@
 import "./CSS/Todoitems.css";
+import tick from "./Assets/tick.png";
+import not_tick from "./Assets/not_tick.png";
+import cross from "./Assets/cross.png";
 
-const Todoitems = () => {
-  return <div>Todoitems</div>;
+const Todoitems = ({ no, display, text }) => {
+  return (
+    <div className="todoitems">
+      <div className="todoitems-container">
+        <img src={not_tick} alt="" />
+        <img src={tick} alt="" />
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img className="todoitems-cross-icon" src={cross} alt="" />
+    </div>
+  );
 };
 
 export default Todoitems;
